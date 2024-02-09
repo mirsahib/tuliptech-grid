2060d274f98598fd587b55bd1e9d337b8458f0fe
Mir Sahib <mirsahib24@gmail.com>
2024-02-09 18:42:01 +0600

    :hammer: Add user input feature

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { Text, View, ViewStyle } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+    Button,
+    Pressable,
+    Text,
+    TextInput,
+    View,
+    ViewStyle,
+} from "react-native";
 
 // Props for the Rectangle component
 interface RectangleProps {
@@ -32,14 +39,20 @@ interface GridProps {
     width: number;
     height: number;
     color?: string;
+    onTouch: (color: string) => void;
 }
 
 // Custom Grid component representing a small rectangle
-const Grid: React.FC<GridProps> = ({ width, height, color }) => {
-    const randomColor =
-        color || "#" + Math.floor(Math.random() * 16777215).toString(16); // Generate a random color if not provided
+const Grid: React.FC<GridProps> = ({ width, height, color, onTouch }) => {
+    const handleTouch = () => {
+        onTouch(color || "");
+    };
 
-    return <View style={{ width, height, backgroundColor: randomColor }} />;
+    return (
+        <Pressable onPressIn={handleTouch}>
+            <View style={{ width, height, backgroundColor: color }} />
+        </Pressable>
+    );
 };
 
 // Main App component
@@ -47,39 +60,141 @@ const App: React.FC = () => {
     // Define the size of the main rectangle
     const rectangleWidth: number = 300;
     const rectangleHeight: number = 400;
-    const rectangleArea = rectangleWidth * rectangleHeight;
+    // Define the minimum cell width and height
+    const minCellWidth = 10;
+    const minCellHeight = 10;
 
-    // Calculate the width and height of each grid cell
-    const cellWidth: number = 40;
-    const cellHeight: number = 100;
-    const cellArea = cellWidth * cellHeight;
+    // Define the width and height of each grid cell
+    const [cellWidth, setCellWidth] = useState<string>("10");
+    const [cellHeight, setCellHeight] = useState<string>("10");
+    // Error message state
+    const [errorMessage, setErrorMessage] = useState<string>("");
 
-    const cellCount = Math.floor(rectangleArea / cellArea);
-    console.log("🚀 ~ cellCount:", cellCount);
+    // Grid state
+    const [grid, setGrid] = useState<React.ReactNode[]>([]);
+    // Grid selected color state
+    const [selectedColor, setSelectedColor] = useState<string>("");
 
-    // Calculate the number of rows and columns
-    const numRows = Math.floor(Math.sqrt(cellCount));
-    const numCols = Math.floor(Math.sqrt(cellCount));
+    // Function to generate a random color
+    const getRandomColor = () => {
+        return "#" + Math.floor(Math.random() * 16777215).toString(16);
+    };
+    // Handle grid touch
+    const handleTouch = (color: string) => {
+        setSelectedColor(color);
+    };
 
-    // Generate the grid of smaller rectangles
-    const grid: React.ReactNode[] = [];
-    for (let i = 0; i < numRows; i++) {
-        for (let j = 0; j < numCols; j++) {
-            grid.push(
-                <Grid key={`${i}-${j}`} width={cellWidth} height={cellHeight} />
+    // Handle submit button press
+    const handleSubmit = () => {
+        const width = parseInt(cellWidth);
+        const height = parseInt(cellHeight);
+
+        // Check if the cell dimensions exceed the maximum rectangle dimensions
+        if (width > rectangleWidth || height > rectangleHeight) {
+            setErrorMessage(
+                `Cell dimensions exceed the rectangle dimensions. Maximum width: ${rectangleWidth}, Maximum height: ${rectangleHeight}`
             );
+            return;
         }
-    }
+
+        // Check if the cell dimensions meet the minimum requirements
+        if (width < minCellWidth || height < minCellHeight) {
+            setErrorMessage(
+                `Minimum width is ${minCellWidth} and minimum height is ${minCellHeight}`
+            );
+            return;
+        }
+
+        // If dimensions are valid, proceed with generating the grid
+        setErrorMessage("");
+
+        const numCol = Math.floor(rectangleWidth / width);
+        const numRow = Math.floor(rectangleHeight / height);
+        const cellCount = numCol * numRow;
+        console.log("🚀 ~ handleSubmit ~ cellCount:", cellCount);
+
+        // Generate the grid of smaller rectangles
+        const newGrid: React.ReactNode[] = [];
+        for (let i = 0; i < numRow; i++) {
+            for (let j = 0; j < numCol; j++) {
+                const color = getRandomColor();
+
+                newGrid.push(
+                    <Grid
+                        key={`${i}-${j}`}
+                        width={width}
+                        height={height}
+                        color={color}
+                        onTouch={handleTouch}
+                    />
+                );
+            }
+        }
+        setGrid(newGrid);
+    };
 
     return (
         <View
             style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
+            <View style={{ flexDirection: "row", marginBottom: 10 }}>
+                <TextInput
+                    style={{
+                        borderWidth: 1,
+                        borderColor: "black",
+                        padding: 5,
+                        marginRight: 10,
+                    }}
+                    placeholder="Cell Width"
+                    value={cellWidth}
+                    onChangeText={setCellWidth}
+                    keyboardType="numeric"
+                />
+                <TextInput
+                    style={{
+                        borderWidth: 1,
+                        borderColor: "black",
+                        padding: 5,
+                        marginRight: 10,
+                    }}
+                    placeholder="Cell Height"
+                    value={cellHeight}
+                    onChangeText={setCellHeight}
+                    keyboardType="numeric"
+                />
+                <Button title="Submit" onPress={handleSubmit} />
+            </View>
+
             <Rectangle
                 style={{ width: rectangleWidth, height: rectangleHeight }}
             >
                 {grid}
             </Rectangle>
+            <View
+                style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginTop: 10,
+                }}
+            >
+                <Text style={{ marginRight: 10 }}>Selected Color:</Text>
+                <TextInput
+                    style={{
+                        borderWidth: 1,
+                        borderColor: "black",
+                        padding: 5,
+                        marginTop: 10,
+                    }}
+                    value={selectedColor}
+                    editable={false}
+                />
+            </View>
+
+            {errorMessage ? (
+                <Text style={{ color: "red", marginVertical: 10 }}>
+                    {errorMessage}
+                </Text>
+            ) : null}
         </View>
     );
 };
